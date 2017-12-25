245bfe02ad7457f22f55555ab1622fdd733e38d5
JosephJvB <jo-van-bo@hotmail.com>
2017-12-25 20:46:59 +1300

    start of ripple lifecycle UI controls

--- client/components/Pool.jsx
+++ client/components/Pool.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+// import Ripple from './Ripple'
+
 class Pool extends React.Component {
   constructor (props) {
     super(props)
@@ -7,11 +9,13 @@ class Pool extends React.Component {
       sx: [],
       sy: [],
       r: 10,
-      cull: ['blue', 'cyan', 'purple', 'pink'],
+      cull: ['blue', 'cyan', 'purple', 'pink', 'blue', 'cyan', 'purple', 'pink', 'blue', 'cyan', 'purple', 'pink'],
       col: null
     }
     this.ripple = this.ripple.bind(this)
     this.clear = this.clear.bind(this)
+    this.changeCol = this.changeCol.bind(this)
+    this.sizeChange = this.sizeChange.bind(this)
   }
 
   clear () {
@@ -20,19 +24,41 @@ class Pool extends React.Component {
 
   ripple (x, y) {
     let { sx, sy, cull } = this.state
-    let raNo = Math.floor(Math.random() * cull.length)
+    // let raNo = Math.floor(Math.random() * cull.length)
     if (sx.length > 13) this.setState({ sx: sx.slice(1, sx.length), sy: sy.slice(1, sy.length) })
-    this.setState({ x: sx.push(x), y: sy.push(y), col: cull[raNo] })
+    this.setState({ x: sx.push(x), y: sy.push(y) })
+  }
+
+  changeCol (c) {
+    this.setState({ col: c })
+  }
+
+  sizeChange (v) {
+    this.setState({ r: this.state.r + v })
   }
 
   render () {
-    const { sy, r, col } = this.state
+    const { sx, sy, r, col } = this.state
     const { h, w } = this.props
     const style = { height: h, width: w }
     return (
-      <div className='pools' style={style} onClick={this.clear} onMouseMove={(e) => { this.ripple(e.pageX, e.pageY) }}>
+      <div className='columns' id='pool' style={style} onClick={this.clear} onMouseMove={(e) => { this.ripple(e.pageX, e.pageY) }}>
+        <div className='column is-1'>
+          <button id='redBut' className='button is-large' onClick={() => this.changeCol('red')}>RED</button>
+          <button id='greenBut' className='button is-large' onClick={() => this.changeCol('green')}>GREEN</button>
+          <button id='blueBut' className='button is-large' onClick={() => this.changeCol('blue')}>BLUE</button>
+          <button id='cyanBut' className='button is-large' onClick={() => this.changeCol('cyan')}>CYAN</button>
+          <button id='yellowBut' className='button is-large' onClick={() => this.changeCol('yellow')}>YELLOW</button>
+          <button id='pinkBut' className='button is-large' onClick={() => this.changeCol('pink')}>PINK</button>
+          <button id='orangeBut' className='button is-large' onClick={() => this.changeCol('orange')}>ORANGE</button>
+          <br />
+          <br />
+          <h1 className='subtitle is-4'>SIZE: </h1>
+          <button className='button' onClick={() => this.sizeChange(5)}>+</button>
+          <button className='button' onClick={() => this.sizeChange(-5)}>-</button>
+        </div>
         <svg style={style}>
-          {this.state.sx.map((x, i) => <circle stroke='cyan' fill='blue' strokeOpacity={0.5} strokeWidth={5} fillOpacity={0.3} key={i} cx={x} cy={sy[i]} r={r * (i * 0.5)} />)}
+          {sx.map((x, i) => <circle key={i} cx={x - 80} cy={sy[i]} r={r * (i * 0.5)} stroke={col} fill={col} strokeOpacity={0.5} strokeWidth={5} fillOpacity={0.3} />)}
         </svg>
       </div>
     )
---
+++ client/components/Ripple.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+class Ripple extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      r: this.props.r
+    }
+    this.embiggen = this.embiggen.bind(this)
+  }
+
+  componentDidMount () {
+    this.embiggen(this.props.r)
+  }
+
+  embiggen (r) {
+    setInterval(this.setState({ r: r * 1.4 }), 10)
+    console.log(this.state.r)
+  }
+
+  render () {
+    const { x, y, c, r } = this.props
+    return (
+      <circle cx={x} cy={y} r={r} stroke={c} fill={c} strokeOpacity={0.5} strokeWidth={5} fillOpacity={0.2} />
+    )
+  }
+}
+
+export default Ripple
