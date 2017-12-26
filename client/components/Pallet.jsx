d6d66eea52a29bb7065bb277042c79c4a06e2699
JosephJvB <jo-van-bo@hotmail.com>
2017-12-27 12:04:22 +1300

    background col and others???

--- client/components/Pallet.jsx
+++ client/components/Pallet.jsx
@@ -4,14 +4,14 @@ const Pallet = (props) => {
   return (
     <div id='pallet'>
       <button id='redBut' className='button is-large' onDoubleClick={() => props.sb('red')} onClick={() => props.cc('red', props.side)}></button>
-      <button id='greenBut' className='button is-large' onDoubleClick={() => props.sb('green')} onClick={() => props.cc('green', props.side)}></button>
+      <button id='greenBut' className='button is-large' onDoubleClick={() => props.sb('rgb(0, 177, 0)')} onClick={() => props.cc('rgb(0, 177, 0)', props.side)}></button>
       <button id='blueBut' className='button is-large' onDoubleClick={() => props.sb('blue')} onClick={() => props.cc('blue', props.side)}></button>
       <button id='cyanBut' className='button is-large' onDoubleClick={() => props.sb('cyan')} onClick={() => props.cc('cyan', props.side)}></button>
       <button id='yellowBut' className='button is-large' onDoubleClick={() => props.sb('yellow')} onClick={() => props.cc('yellow', props.side)}></button>
-      <button id='pinkBut' className='button is-large' onDoubleClick={() => props.sb('pink')} onClick={() => props.cc('pink', props.side)}></button>
+      <button id='pinkBut' className='button is-large' onDoubleClick={() => props.sb('rgb(253, 156, 177)')} onClick={() => props.cc('rgb(253, 156, 177)', props.side)}></button>
       <button id='orangeBut' className='button is-large' onDoubleClick={() => props.sb('orange')} onClick={() => props.cc('orange', props.side)}></button>
       <button id='brownBut' className='button is-large' onDoubleClick={() => props.sb('rgb(143, 74, 22)')} onClick={() => props.cc('rgb(143, 74, 22)', props.side)}></button>
-      <button id='purpleBut' className='button is-large' onDoubleClick={() => props.sb('purple')} onClick={() => props.cc('purple', props.side)}></button>
+      <button id='purpleBut' className='button is-large' onDoubleClick={() => props.sb('rgb(174, 0, 255)')} onClick={() => props.cc('rgb(174, 0, 255)', props.side)}></button>
       <button id='blackBut' className='button is-large' onDoubleClick={() => props.sb('black')} onClick={() => props.cc('black', props.side)}></button>
       <button id='whiteBut' className='button is-large' onDoubleClick={() => props.sb('white')} onClick={() => props.cc('white', props.side)}></button>
       <button id='offBut' className='button is-large' onDoubleClick={() => props.cc('lol', props.side)} onClick={() => props.cc(null, props.side)}>OFF</button>
@@ -22,8 +22,8 @@ const Pallet = (props) => {
       {props.side === 'f' && <p>OPACITY: {Math.ceil(props.fill * 10)}</p>}
       {props.side === 's' && <p>RADIUS: {props.r}</p>}
       <br />
-      <button id='deleteBut' className='button is-large is-success' onClick={() => props.undo()}>U</button>
-      <button id='deleteBut' className='button is-large is-danger' onClick={() => props.clear()}>X</button>
+      {props.side === 'f' && <button id='deleteBut' className='button is-large is-success' onClick={() => props.undo()}>undo</button>}
+      {props.side === 's' && <button id='deleteBut' className='button is-large is-danger' onClick={() => props.clear()}>clear</button>}
     </div>
   )
 }
